fe1e0f68582af9b98544b3769038b9ce34ae2492
Logan Blackstad <logan.blackstad@gmail.com>
2021-01-05 21:06:30 -0500

    add embeeded python code into html

--- flask-blog.py
+++ flask-blog.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Logan B",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "April 20, 2020",
+    },
+    {
+        "author": "Olivia R",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "April 21, 2020",
+    },
+]
 
 
 @app.route("/")
 @app.route("/home")
-def hello():
-    return "<h1>Home Page</h1>"
+def home():
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
